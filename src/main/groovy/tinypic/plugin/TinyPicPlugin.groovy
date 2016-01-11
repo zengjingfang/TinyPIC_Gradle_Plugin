34c5d083d9d2d3f6cc29b72fc7bc8d2ec64b2a21
boyue <boyue@mogujie.com>
2016-01-11 11:30:52 +0800

    add header

--- src/main/groovy/tinypic/plugin/TinyPicPlugin.groovy
+++ src/main/groovy/tinypic/plugin/TinyPicPlugin.groovy
@@ -1,3 +1,14 @@
+/**
+ * Created by boyue
+ * Copyright (c) 2015-present, mogujie.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+
 package tinypic.plugin
 
 import com.android.build.gradle.AppPlugin
